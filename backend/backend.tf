04901d6d71c59dc44e3c90e7a9a814cbe6aad509
mcgigglepop <christoph.alano@gmail.com>
2025-12-17 22:52:52 -0600

    backend

---
+++ backend/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = ""
+    key            = ""
+    region         = ""
+    dynamodb_table = ""
+    encrypt        = true
+    profile        = ""
+  }
+}
